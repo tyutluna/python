506cb35991dc8f54a4887cf8af70a2674e257243
tyutluna <88483363+tyutluna@users.noreply.github.com>
2021-11-25 17:06:48 +1030

    Add files via upload

---
+++ stripping_names.py
@@ -0,0 +1,9 @@
+# store a person's name, and include some whitespace characters at the beginning
+# and the end of the name, make sure you use each character combination at least
+# once.
+
+name = "\tAlbert Einstein\n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
---
+++ stripping_whitespace.py
@@ -0,0 +1,12 @@
+favorite_language = ' python '
+print(favorite_language)
+print(favorite_language.rstrip())
+# rstrip() can remove the whitesapce at right end.
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+# lstrip() can remove the whitespace at light end.
+print(favorite_language.lstrip())
+
+# strip() can remove the whitespace at both side.
+print(favorite_language.strip())
