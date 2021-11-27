a38caf741515d039a83f838d4df5a2ca5466d6aa
tyutluna <88483363+tyutluna@users.noreply.github.com>
2021-11-27 19:02:42 +1030

    Add files via upload

---
+++ bicycles.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialised']
+print(bicycles[0]) # the first element of the list
+print(bicycles)
+print(bicycles[-1]) # the last element of the list
+
+# edit single element
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
