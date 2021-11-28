078a802df59f6138aff6ce6b45406990bf136770
tyutluna <88483363+tyutluna@users.noreply.github.com>
2021-11-29 09:23:14 +1030

    Add files via upload

---
+++ favorite_number.py
@@ -0,0 +1,6 @@
+# store your favorite number in a variable. Then, using that variable create a
+# message that reveals your favorite number.
+
+number = 4
+message = "My favorite number is " + str(number) + ".\n"
+print(message)
