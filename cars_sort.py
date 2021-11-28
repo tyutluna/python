fc3e98fa05410baa27696912c8d8ef511543f987
tyutluna <88483363+tyutluna@users.noreply.github.com>
2021-11-28 19:59:53 +1030

    Add files via upload

---
+++ cars_sort.py
@@ -0,0 +1,22 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# sort a list permanently and alphabetically
+cars.sort()
+print(cars)
+# sort a list in reverse order
+cars.sort(reverse=True)
+print(cars)
+
+# sort a list temporarily
+print(sorted(cars))
+print(cars)
+
+# print a list in reverse order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse()
+print(cars)
+
+# find the length of a list
+length = len(cars)
+print(length)
---
+++ famous_quote.py
@@ -0,0 +1,5 @@
+name = 'albert einstein'
+# avoiding syntax error with strings we should use ingle and double quotes correctly.
+quote = '"A person who never made a mistake never tried anything new."'
+message = name.title() + " once said, " + quote + "\n"
+print(message)
