37400880a8bc3f815b5093250f3bb197e7767235
tyutluna <88483363+tyutluna@users.noreply.github.com>
2021-12-06 22:08:50 +1030

    Add files via upload

---
+++ favorite_languages.py
@@ -0,0 +1,10 @@
+# we can also use a dictionary to store one kind of information about my objects
+favorite_languages = {
+    'jen': 'Python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+# return + tab can break up a long print statement over serval lines.
+print("Sarah's favorite language is " + 
+    favorite_languages['sarah'].title() + ".")
